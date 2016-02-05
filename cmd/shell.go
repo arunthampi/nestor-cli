80d0fc47ddd7c0f0e2b2638466ce8430e3c0afc3
arunthampi <arun.thampi@gmail.com>
2016-02-05 12:51:55 -0800

    Add history,Ctrl-U and other niceties to the shell

--- cmd/shell.go
+++ cmd/shell.go
@@ -16,16 +16,43 @@ package cmd
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+	"path"
 	"strings"
 
-	"github.com/zerobotlabs/nestor-cli/Godeps/_workspace/src/github.com/Bowery/prompt"
 	"github.com/zerobotlabs/nestor-cli/Godeps/_workspace/src/github.com/fatih/color"
+	"github.com/zerobotlabs/nestor-cli/Godeps/_workspace/src/github.com/mitchellh/go-homedir"
+	"github.com/zerobotlabs/nestor-cli/Godeps/_workspace/src/github.com/peterh/liner"
 	"github.com/zerobotlabs/nestor-cli/Godeps/_workspace/src/github.com/spf13/cobra"
 	"github.com/zerobotlabs/nestor-cli/app"
 	"github.com/zerobotlabs/nestor-cli/exec"
 	"github.com/zerobotlabs/nestor-cli/login"
 )
+
+const nestorRoot string = ".nestor"
+const historyFileName string = "history"
+
+var historyFile string
+
+func init() {
+	h, err := homedir.Dir()
+	if err != nil {
+		panic(err)
+	}
+
+	historyFile = path.Join(h, nestorRoot, historyFileName)
+	_, err = os.Stat(historyFile)
+	if err != nil && os.IsNotExist(err) {
+		ioutil.WriteFile(historyFile, []byte(""), 0644)
+	}
+}
+
+func saveHistory(line *liner.State, hf *os.File) {
+	line.WriteHistory(hf)
+	line.Close()
+	hf.Close()
+}
 
 func runShell(cmd *cobra.Command, args []string) {
 	var l *login.LoginInfo
@@ -66,37 +93,54 @@ func runShell(cmd *cobra.Command, args []string) {
 
 	ok := false
 
+	line := liner.NewLiner()
+
+	line.SetCtrlCAborts(true)
+	hf, err := os.OpenFile(historyFile, os.O_RDWR, 0644)
+	if err != nil {
+		color.Red("Unexpected error opening shell\n")
+		line.Close()
+		os.Exit(1)
+	}
+
+	line.ReadHistory(hf)
+
 	for !ok {
-		command, promptErr := prompt.Basic("nestor> ", true)
-		if promptErr != nil {
-			os.Exit(1)
-		}
+		if command, err := line.Prompt("nestor> "); err == nil {
+			command = strings.TrimSpace(command)
+			if command == "quit" || command == "exit" {
+				fmt.Println("Goodbye!")
+				saveHistory(line, hf)
+				os.Exit(1)
+			}
 
-		command = strings.TrimSpace(command)
+			if command == "" {
+				continue
+			}
 
-		if command == "quit" || command == "exit" {
-			fmt.Println("Goodbye!")
-			os.Exit(1)
-		}
+			output := exec.Output{}
+			err := output.Exec(&a, l, command)
+			if err != nil {
+				color.Red("unexpected error while running your app. Please try again later or contact hello@asknestor.me\n", err)
+			}
 
-		output := exec.Output{}
-		err := output.Exec(&a, l, command)
-		if err != nil {
-			color.Red("unexpected error while running your app. Please try again later or contact hello@asknestor.me\n", err)
-		}
+			line.AppendHistory(command)
 
-		if output.Logs != "" {
-			color.Yellow(output.Logs)
-		}
+			if output.Logs != "" {
+				color.Yellow(output.Logs)
+			}
 
-		for _, send := range output.ToSend {
-			fmt.Println(send)
-		}
-		for _, reply := range output.ToReply {
-			color.Blue("<@user>: ")
-			fmt.Println(reply)
+			for _, send := range output.ToSend {
+				fmt.Println(send)
+			}
+			for _, reply := range output.ToReply {
+				color.Blue("<@user>: ")
+				fmt.Println(reply)
+			}
 		}
 	}
+
+	saveHistory(line, hf)
 }
 
 // shellCmd represents the shell command
