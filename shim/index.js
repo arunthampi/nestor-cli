75a2e0bb60bc7308f17bc83ad8166c92aad29cfd
arunthampi <arun.thampi@gmail.com>
2016-02-05 16:10:41 -0800

    Update shim

--- shim/index.js
+++ shim/index.js
@@ -5,25 +5,41 @@ var User = _ref.User;
 var NestorAdapter = _ref.NestorAdapter;
 
 exports.handle = function(event, ctx) {
-  for(var envProp in event.__nestor_env) {
-    process.env[envProp] = event.__nestor_env[envProp];
+  var missingEnv = [];
+
+  for(var requiredEnvProp in event.__nestor_required_env) {
+    if(event.__nestor_required_env[requiredEnvProp] == true &&
+        (!(requiredEnvProp in event.__nestor_env) ||
+         event.__nestor_env[requiredEnvProp] == "")) {
+      missingEnv.push(requiredEnvProp);
+    }
   }
 
-  relaxEvent = event.__relax_event;
-  user = new User(relaxEvent.user_uid,
-                  {
-                    room: relaxEvent.channel_uid
-                  });
-  message = new TextMessage(user, relaxEvent.text);
-  robot = new Robot(relaxEvent.team_uid, relaxEvent.relax_bot_uid, 'nestorbot', event.__debugMode);
-  robot.adapter = new NestorAdapter(robot);
-
-  require('script')(robot);
-
-  robot.receive(message, function(done) {
+  if(missingEnv.length > 0) {
     ctx.succeed({
-      to_send: robot.toSend,
-      to_reply: robot.toReply
+      to_reply: ["You need to set the following environment variables: " + missingEnv.join(', ')]
     });
-  });
+  } else {
+    for(var envProp in event.__nestor_env) {
+      process.env[envProp] = event.__nestor_env[envProp];
+    }
+
+    relaxEvent = event.__relax_event;
+    user = new User(relaxEvent.user_uid,
+                    {
+                      room: relaxEvent.channel_uid
+                    });
+    message = new TextMessage(user, relaxEvent.text);
+    robot = new Robot(relaxEvent.team_uid, relaxEvent.relax_bot_uid, 'nestorbot', event.__debugMode);
+    robot.adapter = new NestorAdapter(robot);
+
+    require('script')(robot);
+
+    robot.receive(message, function(done) {
+      ctx.succeed({
+        to_send: robot.toSend,
+        to_reply: robot.toReply
+      });
+    });
+  }
 }
