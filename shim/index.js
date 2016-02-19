41352f2bcdba9c7ffaba9f59a253df800a5a6e66
arunthampi <arun.thampi@gmail.com>
2016-02-19 12:30:05 -0800

    Remove checking of required env vars from shim
    it's handled in nestorbot@0.14.0

--- shim/index.js
+++ shim/index.js
@@ -7,16 +7,6 @@ var Response = _ref.Response;
 var Message = _ref.Message;
 
 exports.handle = function(event, ctx) {
-  var missingEnv = [];
-
-  for(var requiredEnvProp in event.__nestor_required_env) {
-    if(event.__nestor_required_env[requiredEnvProp] == true &&
-        (!(requiredEnvProp in event.__nestor_env) ||
-         event.__nestor_env[requiredEnvProp] == "")) {
-      missingEnv.push(requiredEnvProp);
-    }
-  }
-
   var relaxEvent = event.__relax_event;
   var user = new User(relaxEvent.user_uid,
                     {
@@ -24,32 +14,22 @@ exports.handle = function(event, ctx) {
                     });
   var msg = new TextMessage(user);
   var robot = new Robot(relaxEvent.team_uid, relaxEvent.relax_bot_uid, event.__debugMode);
+  robot.requiredEnv = event.__nestor_required_env;
 
   for(var envProp in event.__nestor_env) {
     process.env[envProp] = event.__nestor_env[envProp];
   }
 
-  if(missingEnv.length > 0) {
-    var strings = ["You need to set the following environment variables: " + missingEnv.join(', ')];
-    var response = new Response(robot, msg);
-
-    response.reply(strings, function() {
-      ctx.succeed({
-        to_send: robot.toSend
-      });
-    });
-  } else {
-    if(relaxEvent.im == true) {
-      relaxEvent.text = "<@" + relaxEvent.relax_bot_uid + ">: " + relaxEvent.text;
-    }
-
-    message = new TextMessage(user, relaxEvent.text);
-    require('script')(robot);
-
-    robot.receive(message, function(done) {
-      ctx.succeed({
-        to_send: robot.toSend,
-      });
-    });
+  if(relaxEvent.im == true) {
+    relaxEvent.text = "<@" + relaxEvent.relax_bot_uid + ">: " + relaxEvent.text;
   }
+
+  message = new TextMessage(user, relaxEvent.text);
+  require('script')(robot);
+
+  robot.receive(message, function(done) {
+    ctx.succeed({
+      to_send: robot.toSend,
+    });
+  });
 }
